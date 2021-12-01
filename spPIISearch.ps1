0eda5c529f7534780d6108be676b1bbeec12f10a
Austin Livengood <53409880+TheRealGoodLivin@users.noreply.github.com>
2021-12-01 10:37:04 +0100

    Buildable report and parent only scan added

--- spPIISearch.ps1
+++ spPIISearch.ps1
@@ -22,10 +22,11 @@
 #
 
 # CHANGABLE VARIABLES
-$sitePath = "https://usaf.dps.mil/sites/52msg/CS/SCX/IAO/" # SITE PATH
+$sitePath = "https://usaf.dps.mil/sites/52msg/cs/" # SITE PATH
 $reportPath = "C:\users\$env:USERNAME\Desktop\$((Get-Date).ToString("yyyyMMdd_HHmmss"))_SitePIIResults.csv" # REPORT PATH (DEFAULT IS TO DESK
 $results = @() # RESULTS
 $dirtyWords = @("\d{3}-\d{3}-\d{4}","\d{3}-\d{2}-\d{4}","MyFitness","CUI","UPMR","SURF","PA","2583","SF86","SF 86","FOUO","GTC","medical","AF469","AF 469","469","Visitor Request","VisitorRequest","Visitor","eQIP","EPR","910","AF910","AF 910","911","AF911","AF 911","OPR","eval","feedback","loc","loa","lor","alpha roster","alpha","roster","recall","SSN","SSAN","AF1466","1466","AF 1466","AF1566","AF 1566","1566","SGLV","SF182","182","SF 182","allocation notice","credit","allocation","2583","AF 1466","AF1466","1466","AF1566","AF 1566","1566","AF469","AF 469","469","AF 422","AF422","422","AF910","AF 910","910","AF911","AF 911","911","AF77","AF 77","77","AF475","AF 475","475","AF707","AF 707","707","AF709","AF 709","709","AF 724","AF724","724","AF912","AF 912","912","AF 931","AF931","931","AF932","AF 932","932","AF948","AF 948","948","AF 3538","AF3538","3538","AF3538E","AF 3538E","AF2096","AF 2096","2096","AF 2098","AF2098","AF 2098","AF 3538","AF3538","3538","1466","1566","469","422","travel","SF128","SF 128","128","SF 86","SF86","86","SGLV","SGLI","DD214","DD 214","214","DD 149","DD149","149")
+$parentSiteOnly = false;
 
 Connect-PnPOnline -Url $sitePath -UseWebLogin # CONNECT TO SPO
 $subSites = Get-PnPSubWeb -Recurse # GET ALL SUBSITES
@@ -51,11 +52,11 @@ foreach ($DocLib in $getDocLibs) {
                     $loginName = Get-PnPProperty -ClientObject $role.Member -Property LoginName
                     $rolebindings = Get-PnPProperty -ClientObject $role -Property RoleDefinitionBindings
                     $permissions += "$($loginName) - $($rolebindings.Name)"
-                    Write-Host "$($loginName) - $($rolebindings.Name)" -ForegroundColor Yellow
+                    # Write-Host "$($loginName) - $($rolebindings.Name)" -ForegroundColor Yellow
                 }
                 $permissions = $permissions | Out-String
            
-                $results += New-Object PSObject -Property @{
+                $results = New-Object PSObject -Property @{
                     FileName = $subItem["FileLeafRef"]
                     FileExtension = $subItem["File_x0020_Type"]
                     FileSize = $subItem["File_x0020_Size"]
@@ -65,52 +66,65 @@ foreach ($DocLib in $getDocLibs) {
                     Created = $subItem["Created"]
                     Modified = $subItem["Modified"]
                 }
+
+                if (test-path $reportPath) {
+                    $results | Select-Object "FileName", "FileExtension", "FileSize", "Path", "Permissions", "Criteria", "Created", "Modified" | Export-Csv -Path $reportPath -Force -NoTypeInformation -Append
+                } else {
+                    $results | Select-Object "FileName", "FileExtension", "FileSize", "Path", "Permissions", "Criteria", "Created", "Modified" | Export-Csv -Path $reportPath -Force -NoTypeInformation
+                }
             }
         }
     }
 }
 
-# GET ALL SUB SITE DOCUMENT LIBRARIES
-foreach ($site in $subSites) {
-    Connect-PnPOnline -Url $site.Url -UseWebLogin # CONNECT TO SPO SUBSITE
-    $getSubDocLibs = Get-PnPList | Where-Object {$_.BaseTemplate -eq 101}
+if ($parentSiteOnly -eq $false) {
+    # GET ALL SUB SITE DOCUMENT LIBRARIES
+    foreach ($site in $subSites) {
+        Connect-PnPOnline -Url $site.Url -UseWebLogin # CONNECT TO SPO SUBSITE
+        $getSubDocLibs = Get-PnPList | Where-Object {$_.BaseTemplate -eq 101}
 
-    Write-Host "Searching: $($site.Url)" -ForegroundColor Green
+        Write-Host "Searching: $($site.Url)" -ForegroundColor Green
 
-    foreach ($subDocLib in $getSubDocLibs) {
-        $allSubItems = Get-PnPListItem -List $subDocLib -Fields "FileRef", "File_x0020_Type", "FileLeafRef", "File_x0020_Size", "Created", "Modified" 
+        foreach ($subDocLib in $getSubDocLibs) {
+            $allSubItems = Get-PnPListItem -List $subDocLib -Fields "FileRef", "File_x0020_Type", "FileLeafRef", "File_x0020_Size", "Created", "Modified" 
    
-        #LOOP THROUGH EACH DOCMENT IN THE SUB SITES
-        foreach ($subItem in $allSubItems) {
-            foreach ($word in $dirtyWords) {
-                $wordSearch = "(?i)\b$($word)\b"
+            #LOOP THROUGH EACH DOCMENT IN THE SUB SITES
+            foreach ($subItem in $allSubItems) {
+                foreach ($word in $dirtyWords) {
+                    $wordSearch = "(?i)\b$($word)\b"
 
-                if (($subItem["FileLeafRef"] -match $wordSearch)) {
-                    Write-Host "File found. " -ForegroundColor Red -nonewline; Write-Host "Under: '$($word)' Path: $($subItem["FileRef"])" -ForegroundColor Yellow;
+                    if (($subItem["FileLeafRef"] -match $wordSearch)) {
+                        Write-Host "File found. " -ForegroundColor Red -nonewline; Write-Host "Under: '$($word)' Path: $($subItem["FileRef"])" -ForegroundColor Yellow;
 
-                    $permissions = @()
-                    $perm = Get-PnPProperty -ClientObject $subItem -Property RoleAssignments       
-                    foreach ($role in $subItem.RoleAssignments) {
-                        $loginName = Get-PnPProperty -ClientObject $role.Member -Property LoginName
-                        $rolebindings = Get-PnPProperty -ClientObject $role -Property RoleDefinitionBindings
-                        $permissions += "$($loginName) - $($rolebindings.Name)"
-                        Write-Host "$($loginName) - $($rolebindings.Name)" -ForegroundColor Yellow
-                    }
-                    $permissions = $permissions | Out-String
+                        $permissions = @()
+                        $perm = Get-PnPProperty -ClientObject $subItem -Property RoleAssignments       
+                        foreach ($role in $subItem.RoleAssignments) {
+                            $loginName = Get-PnPProperty -ClientObject $role.Member -Property LoginName
+                            $rolebindings = Get-PnPProperty -ClientObject $role -Property RoleDefinitionBindings
+                            $permissions += "$($loginName) - $($rolebindings.Name)"
+                            # Write-Host "$($loginName) - $($rolebindings.Name)" -ForegroundColor Yellow
+                        }
+                        $permissions = $permissions | Out-String
            
-                    $results += New-Object PSObject -Property @{
-                        FileName = $subItem["FileLeafRef"]
-                        FileExtension = $subItem["File_x0020_Type"]
-                        FileSize = $subItem["File_x0020_Size"]
-                        Path = $subItem["FileRef"]
-                        Permissions = $permissions
-                        Criteria = $word
-                        Created = $subItem["Created"]
-                        Modified = $subItem["Modified"]
+                        $results = New-Object PSObject -Property @{
+                            FileName = $subItem["FileLeafRef"]
+                            FileExtension = $subItem["File_x0020_Type"]
+                            FileSize = $subItem["File_x0020_Size"]
+                            Path = $subItem["FileRef"]
+                            Permissions = $permissions
+                            Criteria = $word
+                            Created = $subItem["Created"]
+                            Modified = $subItem["Modified"]
+                        }
+
+                        if (test-path $reportPath) {
+                            $results | Select-Object "FileName", "FileExtension", "FileSize", "Path", "Permissions", "Criteria", "Created", "Modified" | Export-Csv -Path $reportPath -Force -NoTypeInformation -Append
+                        } else {
+                            $results | Select-Object "FileName", "FileExtension", "FileSize", "Path", "Permissions", "Criteria", "Created", "Modified" | Export-Csv -Path $reportPath -Force -NoTypeInformation
+                        }
                     }
                 }
             }
         }
     }
 }
-$results | Select-Object "FileName", "FileExtension", "FileSize", "Path", "Permissions", "Criteria", "Created", "Modified" | Export-Csv -Path $reportPath -Force -NoTypeInformation
